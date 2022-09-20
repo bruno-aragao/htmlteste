45ed6bdfc6a64ba91af36cd5b656e5eae432518a
bruno-aragao <105687878+bruno-aragao@users.noreply.github.com>
2022-09-20 00:28:56 -0300

    Add files via upload

---
+++ exArea.php
@@ -0,0 +1,19 @@
+<html>
+    <body> 
+
+        <h1> Programa de calculo de area de circulo </h1> 
+
+        <?php
+
+        $raio = 2.0; 
+        $pi = 3.14159; 
+
+        $area = ( $raio * $raio ) * $pi;
+
+        printf('A area do circulo resulta em: %s ',number_format($area,4));
+
+        ?> 
+
+    </body>
+    <p> 8128514 - Bruno Aragão Batista </p>
+</html>
